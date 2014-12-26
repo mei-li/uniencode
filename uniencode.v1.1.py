d64f2c177a4ba68aef5200fc39f09ad997bf2a78
Mei Li Triantafyllidi <meili.tr@gmail.com>
2014-12-26 23:04:44 +0100

    removing global var, using constant capitalized convention

--- uniencode.v1.1.py
+++ uniencode.v1.1.py
@@ -26,7 +26,7 @@ except ImportError as e:
     sys.exit(1)
 
 
-default_target_encoding='utf-8'
+DEFAULT_TARGET_ENCODING = 'utf-8'
 
 
 def is_binary(name):
@@ -35,7 +35,7 @@ def is_binary(name):
     return os.system('file "' + name + '" | grep text > /dev/null')
 
 
-def unify_encoding(file_path):
+def unify_encoding(file_path, target_encoding):
     with open(file_path) as fp:
         enc = chardet.detect(fp.read())
 
@@ -47,22 +47,22 @@ def unify_encoding(file_path):
         return False
 
     if confidence <= 0.5:
-        print "Not changing %s file from %s to %s, LOW conf (%s)" % (file_path, encoding, default_target_encoding, confidence)
+        print "Not changing %s file from %s to %s, LOW conf (%s)" % (file_path, encoding, target_encoding, confidence)
         return False
 
-    if confidence > 0.7 and encoding.lower() != default_target_encoding and encoding != 'ascii':
-        print "Changing %s file from %s to %s, with conf %s" % (file_path, encoding, default_target_encoding, confidence)
+    if confidence > 0.7 and encoding.lower() != target_encoding and encoding != 'ascii':
+        print "Changing %s file from %s to %s, with conf %s" % (file_path, encoding, target_encoding, confidence)
         f = codecs.open(file_path, 'r', encoding)
         f2 = open(file_path + 'tmp', 'w')
         try:
             for line in f:
-                f2.write(line.encode(default_target_encoding))
+                f2.write(line.encode(target_encoding))
             f.close()
             f2.close()
             os.remove(file_path)
             os.rename(file_path + 'tmp', file_path)
         except UnicodeEncodeError:
-            print "Changing %s file from %s encoding to %s is NOT possible" % (file_path, encoding, default_target_encoding)
+            print "Changing %s file from %s encoding to %s is NOT possible" % (file_path, encoding, target_encoding)
             f.close()
             f2.close()
             os.remove(file_path+'tmp')
@@ -75,10 +75,10 @@ def unify_encoding(file_path):
             return False
         return True
     if confidence > 0.5 and confidence <= 0.7:
-        print "Changing %s file row by row to %s, with conf %s" % (file_path, default_target_encoding, confidence)
+        print "Changing %s file row by row to %s, with conf %s" % (file_path, target_encoding, confidence)
         problems = False
         f = open(file_path)
-        f2 = open(file_path+'tmp','w')
+        f2 = open(file_path+'tmp', 'w')
         for line in f:
             line_info = chardet.detect(line)
             if (not line_info['encoding']) or line_info['confidence'] < 0.7:
@@ -86,7 +86,7 @@ def unify_encoding(file_path):
                 f2.write(line)
             else:
                 try:
-                    f2.write(unicode(line, line_info['encoding']).encode(default_target_encoding))
+                    f2.write(unicode(line, line_info['encoding']).encode(target_encoding))
                 except (UnicodeDecodeError, UnicodeEncodeError):
                     problems = True
                     f2.write(line)
@@ -99,7 +99,7 @@ def unify_encoding(file_path):
         return True
 
 
-def dtstat(dtroot, pattern):
+def dtstat(dtroot, pattern, target_encoding):
     changed = 0
     for path, dirs, files in os.walk(os.path.abspath(dtroot)):
         for filename in fnmatch.filter(files, pattern):
@@ -110,7 +110,7 @@ def dtstat(dtroot, pattern):
                 if is_binary(file_path):
                     print "Ignoring binary file %s" % file_path
                     continue
-                if unify_encoding(file_path):
+                if unify_encoding(file_path, target_encoding):
                     changed += 1
     print "Changed %s files in total" % changed
 
@@ -144,10 +144,10 @@ def main():
 
     (options, args) = parser.parse_args()
 
-    global default_target_encoding
-
     if options.enc:
-        default_target_encoding = options.enc
+        target_encoding = options.enc
+    else:
+        target_encoding = DEFAULT_TARGET_ENCODING
     if len(args) < 1:
         print "No input file or directory"
         return
@@ -159,11 +159,11 @@ def main():
         if not options.directory:
             file_pathname = os.path.abspath(fname)
             if os.path.isfile(file_pathname):
-                unify_encoding(file_pathname)
+                unify_encoding(file_pathname, target_encoding)
             else:
                 print "Not valid file: %s" % fname
         else:
-            dtstat(fname, options.pattern)
+            dtstat(fname, options.pattern, target_encoding)
     except LookupError, e:
         print e
 
@@ -179,7 +179,7 @@ def test_full_file_one_encoding():
     with open(path, "w") as f:
         f.write(phrase.encode(initial_encoding))
 
-    unify_encoding(path)
+    unify_encoding(path, final_encoding)
 
     with open(path) as f:
         final_phrase = unicode(f.read(), encoding=final_encoding)
@@ -204,7 +204,7 @@ def test_many_encodings_per_file():
         f.write("\n")
         f.write(phrase.encode(final_encoding))
 
-    unify_encoding(path)
+    unify_encoding(path, final_encoding)
     input_phrase = phrase + "\n" + phrase + "\n" + phrase + "\n" + phrase
     with open(path) as f:
         file_content = f.read()
